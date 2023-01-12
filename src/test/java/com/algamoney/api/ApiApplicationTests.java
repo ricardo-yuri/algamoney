ed769b310992e342dcfbd6df5260ae1092235f21
ricardo-yuri <ricardo-yuri@outlook.com>
2023-01-12 13:14:38 -0300

    Initial commit

---
+++ src/test/java/com/algamoney/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algamoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
